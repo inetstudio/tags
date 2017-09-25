fc801370b4888eb7526dd04f9a51e86f64c3fcdc
Konstantin Bukin <bukin@inetstudio.ru>
2017-09-25 23:04:06 +0300

    Add clear images cache event

--- src/Controllers/TagsController.php
+++ src/Controllers/TagsController.php
@@ -211,6 +211,8 @@ class TagsController extends Controller
         foreach ($images as $name) {
             $properties = $request->get($name);
 
+            \Event::fire('inetstudio.images.cache.clear', $name.'_'.md5(get_class($item).$item->id));
+
             if (isset($properties['images'])) {
                 $item->clearMediaCollectionExcept($name, $properties['images']);
 

495f1148645dfd56e0ec61c82c5042b96353d396
Konstantin Bukin <bukin@inetstudio.ru>
2018-04-02 00:47:04 +0300

    Reorganize frontend

--- src/Services/Back/TagsDataTableService.php
+++ src/Services/Back/TagsDataTableService.php
@@ -105,15 +105,15 @@ class TagsDataTableService extends DataTable implements TagsDataTableServiceCont
      */
     protected function getParameters(): array
     {
+        $i18n = trans('admin::datatables');
+
         return [
             'paging' => true,
             'pagingType' => 'full_numbers',
             'searching' => true,
             'info' => false,
             'searchDelay' => 350,
-            'language' => [
-                'url' => asset('/admin/js/plugins/datatables/locales/russian.json'),
-            ],
+            'language' => $i18n,
         ];
     }
 }

fa22acaa9b2d968590c1d5e6730046e93f9ecce5
Konstantin Bukin <bukin@inetstudio.ru>
2020-03-24 19:36:02 +0300

    Update to 7.0

--- entities/tags/src/Contracts/Http/Requests/Back/SaveItemRequestContract.php
+++ entities/tags/src/Contracts/Http/Requests/Back/SaveItemRequestContract.php
@@ -1,12 +1,9 @@
 <?php
 
 namespace InetStudio\TagsPackage\Tags\Contracts\Http\Requests\Back;
-
-use InetStudio\AdminPanel\Base\Contracts\Http\Requests\BaseRequestContract;
-
 /**
  * Interface SaveItemRequestContract.
  */
-interface SaveItemRequestContract extends BaseRequestContract
+interface SaveItemRequestContract
 {
 }
--- entities/tags/src/Contracts/Models/TagModelContract.php
+++ entities/tags/src/Contracts/Models/TagModelContract.php
@@ -3,7 +3,7 @@
 namespace InetStudio\TagsPackage\Tags\Contracts\Models;
 
 use OwenIt\Auditing\Contracts\Auditable;
-use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia;
 use InetStudio\AdminPanel\Base\Contracts\Models\BaseModelContract;
 
 /**

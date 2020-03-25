bb6610d6e8e672a032ea1f0f2c20fc42ec69b64a
Konstantin Bukin <bukin@inetstudio.ru>
2020-03-25 14:51:46 +0300

    Add fields to media select

--- entities/tags/src/Models/TagModel.php
+++ entities/tags/src/Models/TagModel.php
@@ -149,6 +149,8 @@ class TagModel extends Model implements TagModelContract
                         'collection_name',
                         'file_name',
                         'disk',
+                        'conversions_disk',
+                        'uuid',
                         'mime_type',
                         'custom_properties',
                         'responsive_images',
